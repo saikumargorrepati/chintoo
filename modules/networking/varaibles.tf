9e6adddf4ee1db48a3665525a6dbfb60373db7b8
saikumargorrepati <135425720+saikumargorrepati@users.noreply.github.com>
2024-05-22 17:54:18 +0530

    Add files via upload

---
+++ modules/networking/varaibles.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr_block" {}
+variable "vpc_name" {}
+variable "environment" {}
+variable "public_cidr_block" {}
+variable "private_cidr_block" {}
+variable "azs" {}
+variable "natgw_id" {}
+variable "region" {}
